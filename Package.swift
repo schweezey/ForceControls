17e102fbb46eda0c5217c941731344b59f5b66b9
Josh McBroom <josh.mcbroom@gmail.com>
2024-01-16 11:43:17 -0600

    Adding LoadingButton to package

--- Package.swift
+++ Package.swift
@@ -8,14 +8,15 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ForceControls",
-            targets: ["ForceControls"]),
+            name: "LoadingButton",
+            targets: ["LoadingButton"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ForceControls"),
+            name: "LoadingButton",
+            path: "Sources/LoadingButton"),
         .testTarget(
             name: "ForceControlsTests",
             dependencies: ["ForceControls"]),
--- Sources/LoadingButton/LoadingButton.swift
+++ Sources/LoadingButton/LoadingButton.swift
@@ -1,8 +1,49 @@
 //
-//  File.swift
-//  
+//  LoadingButton.swift
+//
 //
 //  Created by Josh McBroom on 1/16/24.
 //
 
-import Foundation
+import SwiftUI
+
+public struct LoadingButton<Content: View>: View {
+    @Binding var isLoading: Bool
+
+    var style: LoadingButtonStyle
+    let content: Content
+    var action: () -> () = {}
+    
+    public init(action: @escaping () -> Void, isLoading: Binding<Bool>, style: LoadingButtonStyle? = nil, @ViewBuilder builder: () -> Content) {
+        self._isLoading = isLoading
+        self.style = style ?? LoadingButtonStyle()
+        content = builder()
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button(action: {
+            if !isLoading {
+                isLoading = true
+                action()
+            }
+        }) {
+            ZStack {
+                Rectangle()
+                    .fill(isLoading ? style.loadingBackgroundColor : style.backgroundColor)
+                    .frame(width: isLoading ? style.height : style.width, height: style.height)
+                    .cornerRadius(isLoading ? style.height/2 : style.cornerRadius)
+
+                if isLoading {
+                    CircleLoadingBar(style: style)
+                }
+                else {
+                    VStack { content }
+                }
+            }
+        }
+        .frame(width: style.width, height: style.height)
+        .disabled(isLoading)
+        .animation(.easeInOut, value: isLoading)
+    }
+}
